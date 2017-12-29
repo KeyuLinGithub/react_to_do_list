2a242e760312f9915dae796274775e871c4df360
Keyu Lin <mlchaellin222@gmail.com>
2017-12-29 14:38:31 -0500

    checkpoint 8 completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,8 +6,9 @@ class ToDo extends Component {
   render() {
      return (
        <li>
-         <input type="checkbox" checked={ this.props.isCompleted } />
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
+         <button onClick={this.props.deleteAction}>delete</button>
        </li>
      );
    }
